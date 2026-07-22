5c50ed015d2578766750e11f7847dfc219c028d5
Andrea Wloch <167016114+shyndry@users.noreply.github.com>
2026-07-22 14:12:40 +0200

    refactor(dao): centralizzata la lookup JNDI del DataSource nella classe ConnessioneDB

--- src/main/java/dao/ConnessioneDB.java
+++ src/main/java/dao/ConnessioneDB.java
@@ -6,22 +6,26 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
+
 public class ConnessioneDB {
     
     private static DataSource ds;
 
-    static{
-        try{
+    static {
+        try {
             InitialContext ctx = new InitialContext();
             ds = (DataSource) ctx.lookup("java:comp/env/jdbc/RacingRelicsDB");
-        } catch (NamingException e){
-            System.err.println("Errore nella configurazione del DS JNDI: " + e.getMessage());
+        } catch (NamingException e) {
+            System.err.println("CRITICAL ERROR: Impossibile trovare il DataSource JNDI 'jdbc/RacingRelicsDB': " + e.getMessage());
         }
     }
 
-    public static Connection getConnection() throws SQLException{
-        if (ds == null ){
-            throw new SQLException("DS non configurato correttamente o non trovato");
+   private ConnessioneDB() {}
+
+   
+    public static Connection getConnection() throws SQLException {
+        if (ds == null) {
+            throw new SQLException("DataSource JNDI non configurato o non trovato nel contesto JNDI.");
         }
         return ds.getConnection();
     }
